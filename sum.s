66a86b6288fd706b7ba941d42206c9f1d0fce376
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2024-03-04 10:56:16 +0700

    fix comments

--- sum.s
+++ sum.s
@@ -10,15 +10,15 @@ main:
     addi x6, x0, 0   # let x6 be sum1 and set it to 0.
     addi x7, x0, 0   # let x7 be sum2 and set it to 0.    
     
-#   for(i = 0; i < size;i++)
-#     arr1[i] = i + 1;
+    # for(i = 0; i < size;i++)
+    #   arr1[i] = i + 1;
     addi x8, x0, 0 # let x8 be i and set it to 0
     la x9, arr1 # loading the address of arr1 to x9
 loop1:
     bge x8 , x5, exit1  # check if i >= size go to exit 1
-#   we need to calculate &arr1[i]
-#   we need the base address of arr1
-#   then we add an offset of i*4 to the base address
+    # we need to calculate &arr1[i]
+    # we need the base address of arr1
+    # then we add an offset of i*4 to the base address
     slli x18,x8, 2   # set x18 to i*4
     add x19, x18, x9 # add i*4 to the base address off arr1 and put it to x19
     addi x20, x8, 1  # set x20 to i + 1
@@ -32,9 +32,9 @@ exit1:
     
 loop2:
     bge x8 , x5, exit2  # check if i >= size go to exit 2
-#   we need to calculate &arr2[i]
-#   we need the base address of arr2
-#   then we aad an offset of i*4 to the base address
+    # we need to calculate &arr2[i]
+    # we need the base address of arr2
+    # then we aad an offset of i*4 to the base address
     slli x18,x8, 2   # set x18 to i*4
     add x19, x18, x21 # add i*4 to the base address off arr2 and put it to x19
     add x20, x8, x8  # set x20 to i + i (2*i)
@@ -53,7 +53,7 @@ loop3:
     add x19, x18, x9 # add i*4 to the base address off arr1 and put it to x19
     lw x20, 0(x19) # x20 has arr[i]
     add x6, x6, x20 # sum1 = sum1 + arr[i]
-#     sum2 + arr2[i]
+    # sum2 + arr2[i]
     add x19, x18, x21 #
     lw x20, 0(x19) # x20 has arr2[i]
     add x7, x7, x20 # sum2 = sum2 + arr2[i]
